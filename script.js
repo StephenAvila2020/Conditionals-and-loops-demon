a1005af885e9b9b2039193874240bdbeba2b02c9
Stephen <stephen.r.avila@gmail.com>
2020-03-21 11:18:38 -0400

    Demo code for conditionals

---
+++ script.js
@@ -0,0 +1,16 @@
+var likesToTravel = true;
+
+if (likesToTravel === true) {
+    console.log("Bon Voyage")
+} else {
+    console.log("Ha loser") 
+}
+
+
+var favoriteFood = "fried pickles";
+
+if (favoriteFood === "fried pickles" || favoriteFood === "tacos") {
+    console.log("You have great taste")
+} else {
+console.log("Do you even have tastebuds?")
+}
