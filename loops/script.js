8ed6fc6f105a3bc17e96df72128ba741891bceab
Stephen <stephen.r.avila@gmail.com>
2020-03-21 14:27:33 -0400

    loops

---
+++ loops/script.js
@@ -0,0 +1,19 @@
+// var birdCounter = 0;
+
+// while(birdCounter <= 100){
+//     console.log("Tweet Tweet")
+//     birdCounter = birdCounter + 1;
+// }
+
+// // For loop
+
+// for(var i = 0; i <= 100; i++){
+//     console.log("Tweet Tweet")
+// }
+
+var favoriteMovies = ["Endgame", "Infinity War", "Civil War", "Winter Soldier"]
+
+for(var i=0; i < 4; i++){
+    console.log("This is i", i)
+    console.log('I love ${favoriteMovies[i}')
+}
